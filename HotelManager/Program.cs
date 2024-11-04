b71b64f90712f85bc8e05cde61c3d27bd0aaa066
JUKKarol <karolprzybylak2@gmail.com>
2024-11-04 22:17:25 +0100

    Read data from json to console

--- HotelManager/Program.cs
+++ HotelManager/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace HotelManager;
+﻿using System.Text.Json;
+using System;
+using HotelManager.Entities;
+using HotelManager.Services;
+
+namespace HotelManager;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string workingDirectory = Environment.CurrentDirectory;
+        string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName;
+
+        string hotelsJson = Path.Combine(projectDirectory, "hotels.json");
+
+        var hotelService = new HotelService();
+
+        List<Hotel> hotels = hotelService.GetHotels(hotelsJson);
+
+        foreach (var hotel in hotels)
+        {
+            Console.WriteLine($"Hotel: {hotel.Name}");
+
+            foreach (var room in hotel.Rooms)
+            {
+                Console.WriteLine($"Room: {room.RoomId}");
+            }
+        }
     }
 }
---
+++ HotelManager/Services/HotelService.cs
@@ -0,0 +1,34 @@
+﻿using HotelManager.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HotelManager.Services;
+
+internal class HotelService
+{
+    public List<Hotel> GetHotels(string jsonPath)
+    {
+        try
+        {
+            string jsonData = File.ReadAllText(jsonPath);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            List<Hotel> hotels = JsonSerializer.Deserialize<List<Hotel>>(jsonData, options);
+
+            return hotels;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return null;
+        }
+    }
+}
